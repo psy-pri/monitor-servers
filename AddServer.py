eef4d1d04bee511578d4cbac23da2970cdae970e
Shukla <priyashukla@deloitte.com>
2021-10-04 20:41:04 +0530

    first commit

---
+++ AddServer.py
@@ -0,0 +1,20 @@
+'''
+This functionality is created to add a new server to the pickle file without deleting it
+'''
+
+import pickle 
+from CheckServer import Server
+
+servers = pickle.load(open("servers.pickle","rb"))
+
+print("Add server")
+
+server_name = input("Enter server name ")
+port = input("Enter port number ")
+connection = input("Enter connection type: plain/ssl/ping ")
+priority = input("Enter priority: high/low ")
+
+new_server = Server(server_name, port, connection, priority)
+servers.append(new_server)
+
+pickle.dump(servers, open("servers.pickle", "wb"))
---
+++ CheckServer.py
@@ -0,0 +1,95 @@
+import socket 
+import ssl
+from datetime import date, datetime
+import pickle #save runtime info in a pickle file after py file execution 
+
+import subprocess #helps with firewall connectivity issues in servers 
+import platform #helps to know the platform like windows or linux
+
+
+'''
+class def : This class represents a server
+
+params-
+name: server name/ IP address 
+port: server port to connect to
+connection: type of connection - ssl,ping
+priority: priority type - low,high 
+
+history - stores history of server
+alert - check flag to send alert only once in case the server is down
+'''
+
+class Server():
+    def __init__(self, name, port, connection, priority):
+        self.name = name
+        self.port = port 
+        self.connection = connection.lower()
+        self.priority = priority.lower()
+        
+        self.history = [] 
+        self.alert = False 
+    
+    def check_connection(self):
+        msg = ""
+        success = False
+        now = datetime.now()
+
+        try:
+            if self.connection == "plain":
+                socket.create_connection((self.name,self.port),timeout=10)
+                msg = "{} is up, on port {}".format(self.name,self.port)
+                success = True
+                self.alert = False
+            elif self.connection == "ssl":
+                socket.create_connection((self.name,self.port), timeout=10)
+                msg = "{} is up, on port {}".format(self.name,self.port)
+                success = True
+                self.alert = False
+            else:
+                if self.ping():
+                    msg = "{} is up, on port {}".format(self.name,self.port)
+                    success = True
+                    self.alert = False
+        except socket.timeout: #connection is not refused but is not established as well
+            msg = "server: {} timeout, on port {}".format(self.name,self.port)
+        except (ConnectionRefusedError, ConnectionResetError) as e:
+            msg = "server: {} {}".format(self.name,e)
+        except Exception as e:
+            msg = "No clue?? {}".format(e)
+    
+        self.create_history(msg,success,now)
+
+    def create_history(self, msg, success, now):
+        history_max  = 100
+        self.history.append((msg,success,now))
+        
+        while len(self.history) > history_max:
+            self.history.pop(0)
+
+    def ping(self):
+        try:
+            output = subprocess.check_output("ping -{} 1 {}".format('n' 
+            if platform.system().lower() == "windows"
+            else "c",self.name ), shell=True, universal_newlines=True)
+            if "unreachable" in output:
+                return False
+            else:
+                return True 
+        except Exception:
+            return False
+
+if __name__ == "__main__":
+    try:
+        servers = pickle.load(open("servers.pickle","rb"))
+    except:
+        servers = [
+            Server("google.com", 80, "plain", "high"),
+            Server("smtp.gmail.com", 465, "ssl", "high"),
+            Server("reddit.com", 80, "plain","high")]
+
+    for server in servers:
+        server.check_connection()
+        print(len(server.history))
+
+    pickle.dump(servers, open("servers.pickle", "wb")) #pickle.dump(what u want to save, open(filename.pickle, write mode))
